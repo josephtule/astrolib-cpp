14c351cc09402f25d28821bcea880dcd6ccbf55d
Joseph Le <letujoseph@gmail.com>
2026-05-08 00:50:14 -0700

    now using tle struct for reading, still no multiread

--- include/core/ingest.hpp
+++ include/core/ingest.hpp
@@ -3,41 +3,52 @@
 #include "core/body.hpp"
 #include "core/time.hpp"
 #include "util/units.hpp"
-#include "util/vecdefs.hpp"
 
-// TLE
+// TLE (Earth only)
 struct TLEData {
     // computed values
     JulianDate jd_utc;
-    f64 eccen_anom;
+    f64 sma = 0.0;
+    f64 eccen_anom = 0.0;
+    f64 ta = 0.0;
 
     // line 0
-    std::string name;
+    std::string name = "";
 
     // line 1
-    i32 sat_num;
-    i32 launch_year;
-    i32 launch_num;
-    std::string launch_piece;
-    i32 epoch_year;
-    f64 epoch_day_frac;
-    f64 d_mean_motion;
-    f64 dd_mean_motion;
-    f64 b_star;
-    i32 ephemeris_type;
-    i32 element_set_number;
+    i32 sat_num = 0;
+    i32 launch_year = 0;
+    i32 launch_num = 0;
+    std::string launch_piece = "";
+    i32 epoch_year = 0;
+    f64 epoch_day_frac = 0.0;
+    f64 d_mean_motion = 0.0;
+    f64 dd_mean_motion = 0.0;
+    f64 b_star = 0.0;
+    i32 ephemeris_type = 0;
+    i32 element_set_number = 0;
 
     // line 2
-    f64 inc;
-    f64 raan;
-    f64 ecc;
-    f64 aop;
-    f64 mean_anom;
-    f64 mean_motion;
-    i32 rev;
+    f64 inc = 0.0;
+    f64 raan = 0.0;
+    f64 ecc = 0.0;
+    f64 aop = 0.0;
+    f64 mean_anom = 0.0;
+    f64 mean_motion = 0.0;
+    i32 rev = 0;
 
-    UAngle units_angle;
+    UAngle units_angle = UAngle::radian;
 };
+
+bool read_TLE_single(
+    const std::string& filename,
+    TLEData& tle,
+    f64 mu,
+    i32 millenium,
+    i32 lineskips,
+    i32 skip_sats,
+    UAngle angle_out = UAngle::radian
+);
 
 bool read_TLE_single(
     const std::string& filename,
@@ -72,3 +83,6 @@ bool read_TLE_index(
     i32 lineskips = 0,
     UAngle angle_out = UAngle::radian
 );
+
+void sat_from_tle_data(Satellite &sat, const TLEData &tle, f64 mu);
+Satellite sat_from_tle_data(const TLEData& tle, f64 mu);
--- src/ingest.cpp
+++ src/ingest.cpp
@@ -1,18 +1,66 @@
 #include "core/ingest.hpp"
+#include "core/body.hpp"
 #include "core/orbital_elements.hpp"
 #include "core/time.hpp"
 #include "util/astrodynamics.hpp"
+#include "util/typedefs.hpp"
 #include "util/units.hpp"
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 
 // TODO: use TLEData struct
 
+bool read_line_one(std::string line, TLEData& tle) {
+    try {
+        tle.sat_num = std::stoi(line.substr(2, 5));
+        tle.launch_year = std::stoi(line.substr(9, 2));
+        tle.launch_num = std::stoi(line.substr(11, 3));
+        tle.launch_piece = line.substr(14, 3);
+        tle.epoch_year = std::stoi(line.substr(18, 2));
+        tle.epoch_day_frac = std::stod(line.substr(20, 12));
+        tle.d_mean_motion = std::stod(line.substr(33, 10));
+        tle.dd_mean_motion = std::stod("0." + line.substr(44, 6))
+                             * std::pow(10, std::stod(line.substr(50, 2)));
+        tle.b_star = std::stod("0." + line.substr(54, 6))
+                     * std::pow(10, std::stod(line.substr(59, 2)));
+
+        tle.ephemeris_type = std::stoi(line.substr(62, 1));
+        tle.element_set_number = std::stoi(line.substr(64, 4));
+    } catch (const std::invalid_argument&) {
+        return false;
+    } catch (const std::out_of_range&) {
+        return false;
+    }
+
+    return true;
+}
+
+bool read_line_two(std::string line, TLEData& tle) {
+    try {
+        i32 sat_num_2 = std::stoi(line.substr(2, 5));
+        if (tle.sat_num != sat_num_2) return false;
+
+        tle.inc = std::stod(line.substr(8, 8));
+        tle.raan = std::stod(line.substr(17, 8));
+        tle.ecc = std::stod("0." + line.substr(26, 7));
+        tle.aop = std::stod(line.substr(34, 8));
+        tle.mean_anom = std::stod(line.substr(43, 8));
+        tle.mean_motion = std::stod(line.substr(52, 11));
+        tle.rev = std::stoi(line.substr(63, 5));
+    } catch (const std::invalid_argument&) {
+        return false;
+    } catch (const std::out_of_range&) {
+        return false;
+    }
+
+    return true;
+}
+
 bool read_TLE_single(
     const std::string& filename,
-    Satellite& sat,
-    JulianDate& jd_utc,
+    TLEData& tle,
     f64 mu,
     i32 millenium,
     i32 lineskips,
@@ -27,32 +75,6 @@ bool read_TLE_single(
 
     std::string line;
     for (i32 i = 0; i < lineskips && std::getline(file, line); ++i) {}
-
-    // common tle fields
-    // line 0
-    std::string name;
-
-    // line 1
-    i32 sat_num;
-    i32 launch_year;
-    i32 launch_num;
-    std::string launch_piece;
-    i32 epoch_year;
-    f64 epoch_day_frac;
-    f64 d_mean_motion;
-    f64 dd_mean_motion;
-    f64 b_star;
-    i32 ephemeris_type;
-    i32 element_set_number;
-
-    // line 2
-    f64 inc;
-    f64 raan;
-    f64 ecc;
-    f64 aop;
-    f64 mean_anom;
-    f64 mean_motion;
-    i32 rev;
 
     bool found_sat = false;
 
@@ -75,9 +97,9 @@ bool read_TLE_single(
         case 0: {
             if (sats_seen == skip_sats) {
                 if (line.size() > 2 && line[0] == '0')
-                    name = line.substr(2);
+                    tle.name = line.substr(2);
                 else
-                    name = line;
+                    tle.name = line;
             }
         } break;
 
@@ -85,22 +107,7 @@ bool read_TLE_single(
             have_line1 = true;
 
             if (sats_seen == skip_sats) {
-                sat_num = std::stoi(line.substr(2, 5));
-                launch_year = std::stoi(line.substr(9, 2));
-                launch_num = std::stoi(line.substr(11, 3));
-                launch_piece = line.substr(14, 3);
-                epoch_year = std::stoi(line.substr(18, 2));
-                epoch_day_frac = std::stod(line.substr(20, 12));
-                d_mean_motion = std::stod(line.substr(33, 10));
-
-                dd_mean_motion = std::stod(line.substr(44, 6)) / std::pow(10, 5)
-                                 * std::pow(10, std::stod(line.substr(50, 2)));
-
-                b_star = std::stod(line.substr(54, 6)) / std::pow(10, 5)
-                         * std::pow(10, std::stod(line.substr(59, 2)));
-
-                ephemeris_type = std::stoi(line.substr(62, 1));
-                element_set_number = std::stoi(line.substr(64, 4));
+                if (!read_line_one(line, tle)) return false;
             }
         } break;
 
@@ -108,16 +115,7 @@ bool read_TLE_single(
             if (!have_line1) return false;
 
             if (sats_seen == skip_sats) {
-                i32 sat_num_2 = std::stoi(line.substr(2, 5));
-                if (sat_num != sat_num_2) return false;
-
-                inc = std::stod(line.substr(8, 8));
-                raan = std::stod(line.substr(17, 8));
-                ecc = std::stod(line.substr(26, 7)) / std::pow(10, 7);
-                aop = std::stod(line.substr(34, 8));
-                mean_anom = std::stod(line.substr(43, 8));
-                mean_motion = std::stod(line.substr(52, 11));
-                rev = std::stoi(line.substr(63, 5));
+                if (!read_line_two(line, tle)) return false;
 
                 found_sat = true;
                 break;
@@ -136,15 +134,14 @@ bool read_TLE_single(
     if (!found_sat) return false;
 
     // convert from rev/day^i to deg/s^i, assume 86400s day
-    mean_motion *= 360.0 / 86400.0;
-    d_mean_motion *= 360.0 / std::pow(86400.0, 2);
-    dd_mean_motion *= 360.0 / std::pow(86400.0, 3);
+    tle.mean_motion *= 360.0 / 86400.0;
+    tle.d_mean_motion *= 360.0 / std::pow(86400.0, 2);
+    tle.dd_mean_motion *= 360.0 / std::pow(86400.0, 3);
 
-    f64 eccen_anom;
     bool eccen_anom_ok = mean_anom_to_eccen_anom(
-        mean_anom,
-        ecc,
-        eccen_anom,
+        tle.mean_anom,
+        tle.ecc,
+        tle.eccen_anom,
         UAngle::degree,
         UAngle::radian
     ); // rad
@@ -152,11 +149,11 @@ bool read_TLE_single(
 
     // true anomaly
     f64 ta = std::atan2(
-        std::sqrt(1.0 - ecc * ecc) * std::sin(eccen_anom),
-        std::cos(eccen_anom) - ecc
+        std::sqrt(1.0 - tle.ecc * tle.ecc) * std::sin(tle.eccen_anom),
+        std::cos(tle.eccen_anom) - tle.ecc
     );
     f64 n = convert_angle(
-        mean_motion,
+        tle.mean_motion,
         UAngle::degree,
         UAngle::radian
     ); // mean motion in rad/s
@@ -165,23 +162,38 @@ bool read_TLE_single(
     // TLE data is in degrees
     UAngle angle_in = UAngle::degree;
     if (angle_out != UAngle::degree) {
-        inc = convert_angle(inc, angle_in, angle_out);
-        raan = convert_angle(raan, angle_in, angle_out);
-        aop = convert_angle(aop, angle_in, angle_out);
-        mean_anom = convert_angle(mean_anom, angle_in, angle_out);
-        mean_motion = convert_angle(mean_motion, angle_in, angle_out);
-        d_mean_motion = convert_angle(d_mean_motion, angle_in, angle_out);
-        dd_mean_motion = convert_angle(dd_mean_motion, angle_in, angle_out);
+        tle.inc = convert_angle(tle.inc, angle_in, angle_out);
+        tle.raan = convert_angle(tle.raan, angle_in, angle_out);
+        tle.aop = convert_angle(tle.aop, angle_in, angle_out);
+        tle.mean_anom = convert_angle(tle.mean_anom, angle_in, angle_out);
+        tle.mean_motion = convert_angle(tle.mean_motion, angle_in, angle_out);
+        tle.d_mean_motion = convert_angle(tle.d_mean_motion, angle_in, angle_out);
+        tle.dd_mean_motion = convert_angle(tle.dd_mean_motion, angle_in, angle_out);
     }
+    tle.units_angle = angle_out;
 
-    CalendarTime cal = doy_to_cal(epoch_day_frac, millenium + epoch_year);
-    jd_utc = cal_to_jd(cal);
+    CalendarTime cal = doy_to_cal(tle.epoch_day_frac, millenium + tle.epoch_year);
+    tle.jd_utc = cal_to_jd(cal);
 
-    OEClassical
-        coe{.sma = sma, .ecc = ecc, .inc = inc, .raan = raan, .aop = aop, .ta = ta};
+    return true;
+}
+bool read_TLE_single(
+    const std::string& filename,
+    Satellite& sat,
+    JulianDate& jd_utc,
+    f64 mu,
+    i32 millenium,
+    i32 lineskips,
+    i32 skip_sats,
+    UAngle angle_out
+) {
+    TLEData tle;
+    bool read_ok
+        = read_TLE_single(filename, tle, mu, millenium, lineskips, skip_sats, angle_out);
+    if (!read_ok) return false;
 
-    sat.name = name;
-    sat.x_tr = classical_to_rv(coe, mu, angle_out);
+    sat = sat_from_tle_data(tle, mu);
+    jd_utc = tle.jd_utc;
 
     return true;
 }
@@ -205,4 +217,41 @@ bool read_TLE_multiple(
 
     std::string line;
     for (i32 i = 0; i < lineskips && std::getline(file, line); ++i) {}
+}
+
+bool read_TLE_index(
+    const std::string& filename,
+    svec<Satellite>& sats,
+    svec<JulianDate>& jds,
+    const svec<i32>& idx,
+    f64 mu,
+    i32 millenium,
+    i32 lineskips,
+    UAngle angle_out
+) {
+    svec<i32> idx_sorted = idx;
+    std::sort(idx_sorted.begin(), idx_sorted.end());
+
+    return true;
+}
+
+void sat_from_tle_data(Satellite& sat, const TLEData& tle, f64 mu) {
+    OEClassical coe{
+        .sma = tle.sma,
+        .ecc = tle.ecc,
+        .inc = tle.inc,
+        .raan = tle.raan,
+        .aop = tle.aop,
+        .ta = tle.ta
+    };
+
+    sat.name = tle.name;
+    sat.x_tr = classical_to_rv(coe, mu, tle.units_angle);
+}
+
+Satellite sat_from_tle_data(const TLEData& tle, f64 mu) {
+    Satellite sat;
+    sat_from_tle_data(sat, tle, mu);
+
+    return sat;
 }
